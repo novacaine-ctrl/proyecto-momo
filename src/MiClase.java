7f43b88ec3ec5826f16eb2f09bf098372e148d18
novacaine-ctrl <jg347550@gmail.com>
2026-08-25 19:35:32 -0500

    primer commit

--- src/MiClase.java
+++ src/MiClase.java
@@ -16,7 +16,53 @@ public class MiClase {
     System.out.println(a / b);
     System.out.println(a % b);
 
-   
+   System.out.print("Numero 1: ");
+   double a = sc.nextDouble();
+   System.out.print("Numero 2: ");
+    double b = sc.nextDouble();
+
+    System.out.print("Operacion (+, -, *, /): ");
+    char operacion = sc.next().charAt(0);
+
+if (operacion == '+') {
+    resultado = a + b;
+} else if (operacion == '-') {
+    resultado = a - b;
+} else if (operacion == '*') {
+    resultado = a * b;
+} else if (operacion == '/') {
+    if (num2 != 0) {
+        resultado = num1 / num2;
+    } else {
+        System.out.println("Error: División por cero");
+        return;
+    }
+} else {
+    System.out.println("Operación no válida");
+    return;
+
+
+    for (int i = 1; i <= 10; i++) {
+        System.out.println("Tabla del " + i);
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(i + " x " + j + " = " + (i * j));
+        }
+        System.out.println();
+    }
+
+    for (int i = 0; i <= 15; i++) 
+
+
+
+
+    public class TablaMultiplicar {}    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            system.out.print("¿De que numero quieres la tabla?: ");
+            int numero = sc.nextInt();
+
+            system.out.println("")
 
 
     
